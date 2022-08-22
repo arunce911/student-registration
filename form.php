33c35190d1978aa042091423c4500eea263fa184
Arūnas <107066083+arunce911@users.noreply.github.com>
2022-08-22 11:54:15 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form</title>
+    <title>Studentu registracija</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
@@ -28,6 +28,7 @@
                     <label for="pavarde">Pavarde</label>
                 </div>
             </div>
+
             <p>Lytis:</p>
 
             <div class="form-check form-check-inline">
@@ -83,10 +84,6 @@
             </div>
 
             <button type="submit" name="submit">Įrašyti</button>
-
-
-
-
         </div>
     </form>
 
